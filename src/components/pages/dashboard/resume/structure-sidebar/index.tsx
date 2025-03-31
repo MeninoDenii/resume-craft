32aa81f6c6916cf54b25988a79d6aa33b282423f
MeninoDenii <zapnot14@gmail.com>
2025-03-31 16:04:57 -0300

    finalizada seção de layout

--- src/components/pages/dashboard/resume/structure-sidebar/index.tsx
+++ src/components/pages/dashboard/resume/structure-sidebar/index.tsx
@@ -1,11 +1,13 @@
 import { Separator } from "@/components/ui/separator";
 import { TemplatesListSection } from "./sections/template-list";
+import { LayoutSection } from "./sections/layout";
 
 export const StructureSidebar = () => {
   return (
     <aside className="w-full h-full p-6 overflow-y-auto">
       <TemplatesListSection />
       <Separator className="my-5" />
+      <LayoutSection />
     </aside>
   );
 };
