7a7833bda8b0a176c65f2f5a0ed5e9a159bba006
MeninoDenii <zapnot14@gmail.com>
2025-04-02 11:38:05 -0300

    finalizada criação do elemento arrastável

--- src/components/pages/dashboard/resume/resume-content/index.tsx
+++ src/components/pages/dashboard/resume/resume-content/index.tsx
@@ -1,4 +1,9 @@
+"use client";
 import { cn } from "@/lib/utils";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { ResumeTemplate } from "./templates";
+import { TransformControls } from "./controls";
+import { NavigationHeader } from "./header";
 
 export const ResumeContent = () => {
   return (
@@ -7,6 +12,22 @@ export const ResumeContent = () => {
         "overflow-hidden w-full h-full flex items-center justify-center",
         "relative bg-muted dark:bg-background"
       )}
-    ></section>
+    >
+      <TransformWrapper
+        initialScale={0.5}
+        minScale={0.4}
+        centerOnInit
+        centerZoomedOut
+        limitToBounds={false}
+      >
+        <>
+          <NavigationHeader />
+          <TransformControls />
+          <TransformComponent>
+            <ResumeTemplate />
+          </TransformComponent>
+        </>
+      </TransformWrapper>
+    </section>
   );
 };
